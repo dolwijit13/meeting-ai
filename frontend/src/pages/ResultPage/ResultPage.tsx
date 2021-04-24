eb63955050e45b48ce800674b3207293f26c8914
Sumet Chorthongdee <sumetchor@gmail.com>
2021-04-24 14:00:48 +0700

    entitries table

--- frontend/src/pages/ResultPage/ResultPage.tsx
+++ frontend/src/pages/ResultPage/ResultPage.tsx
@@ -11,6 +11,26 @@ export const ResultPage: React.FC<IResultPage> = (props: any) => {
   const location = useLocation<any>();
   const { rekognition } = location.state;
 
+  const entities = {
+    "Entities": [
+      {
+        "Text": "today",
+        "Score": 0.97,
+        "Type": "DATE",
+        "BeginOffset": 14,
+        "EndOffset": 19
+      },
+      {
+        "Text": "Seattle",
+        "Score": 0.95,
+        "Type": "LOCATION",
+        "BeginOffset": 23,
+        "EndOffset": 30
+      }
+    ],
+    "LanguageCode": "en"
+  };
+
   const languageChoices = languages.map((lang, idx) => (
     <div className={styles.langArea}>
       <div>{idx > 0 && '|'}</div>
@@ -30,6 +50,28 @@ export const ResultPage: React.FC<IResultPage> = (props: any) => {
       </div>
       <div className={styles.emotionSection}>
         <div className={styles.label}>Emotions: {JSON.stringify(rekognition)} </div>
+      </div>
+      <div className={styles.entitiesSection}>
+        <div className={styles.label}>
+          Entities:
+          <table id='entitries'>
+            <tbody>
+              <tr>
+                <th style={{width: "100px"}}>{'Type'}</th>
+                <th>{'Text'}</th>
+              </tr>
+              {entities["Entities"].map((entity, index) => {
+                const { Text, Type } = entity
+                return (
+                  <tr key={index}>
+                    <td>{Type}</td>
+                    <td>{Text}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className={styles.recordSection}>
         <div className={styles.label}>Record:</div>
